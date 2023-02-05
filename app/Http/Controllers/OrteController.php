1e86767eb88bf83fc69de867342b28b4279f32c6
hboff <oskar@baucampus.de>
2023-02-05 13:51:23 +0100

    packages

--- app/Http/Controllers/OrteController.php
+++ app/Http/Controllers/OrteController.php
@@ -3,12 +3,6 @@
 namespace App\Http\Controllers;
 
 
-use App\Models\Ort;
-
-use Illuminate\Http\Request;
-use Illuminate\Validation\Rule;
-use Illuminate\Support\Facades\DB;
-
 class OrteController extends Controller
 {
     // Show single lisitng
