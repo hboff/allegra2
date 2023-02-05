1fcd0c532de1f2d42ceb0206fb55de4f375d48cd
hboff <oskar@baucampus.de>
2023-02-05 13:52:02 +0100

    Install

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrteController;
+use App\Providers\RouteServiceProvider;
+use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
